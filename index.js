0b22e1e1f9b36c2043c3dc73ec0b3817339d6f27
Srijan Chaudhary <srijan@unimelb.edu.au>
2019-12-18 13:45:58 +1100

    Integrating Actions

--- index.js
+++ index.js
@@ -2,11 +2,17 @@
 
 // Creating store
 const redux = require('redux')
+const reduxLogger = require('redux-logger')
+
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
 
 // console.log("From index.js")
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 // Define Action
 // {
@@ -22,14 +28,30 @@ function buyCake() {
     }
 }
 
+function buyIceCream() {
+    return {
+        type: BUY_ICECREAM,
+        info: 'Second redux action'
+    }
+}
+
 // Reducer
 // (previousState, action) => newState
 
-const initialState = {
+// const initialState = {
+//     numOfCakes: 10,
+//     numOfIceCreams: 20
+// }
+
+const initialCakeState = {
     numOfCakes: 10
 }
 
-const reducer = (state = initialState, action) => {
+const initialIceCreamState = {
+    numOfIceCream: 10
+}
+
+const cakeReducer = (state = initialCakeState, action) => {
     switch(action.type) {
         case BUY_CAKE: return {
             ... state,      // making copy of state object
@@ -40,13 +62,34 @@ const reducer = (state = initialState, action) => {
     }
 }
 
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch(action.type) {
+        case BUY_ICECREAM: return {
+            ... state,      // making copy of state object
+            numOfIceCreams: state.numOfIceCreams - 1
+        }
+
+        default: return state
+    }
+}
+
+// Combining reducers
+const rootReducer = combineReducers ({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+})
+
 // Creating store
-const store = createStore(reducer)
+const store = createStore(rootReducer, applyMiddleware(logger))      // Can accept only one reducer
 console.log('Initial State: ', store.getState());
 const unsubscribe = store.subscribe(() => console.log('Updated State: ', store.getState()))
 
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 
 unsubscribe()
